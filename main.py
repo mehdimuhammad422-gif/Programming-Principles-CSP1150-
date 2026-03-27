b336913777514a8ecb50ce6710605fb2e9891ebb
mehdimuhammad422-gif <mehdimuhammad422@gmail.com>
2026-03-27 16:45:58 +0500

    Adding loop untill the input becomes valid

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ print ('Enter your roll number: ')
 rollNo=input()
 print(f'Welcome to {s_name} ({rollNo}) Number List Test')
 print('Select the difficulty level [E]asy, [M]edium, [H]ard. ')
-level=input()
-if level=='E' or level=='e':
-    print('Easy')
-elif level=='M' or level=='m':
-    print('Medium') 
-elif level=='H' or level=='h':
-    print('Hard')
-else:
-    print('Invalid input. Please select E, M, or H.')
+level=''
+while level not in ['E', 'e', 'M', 'm', 'H', 'h']:
+    level=input( )
+    if level=='E' or level=='e':
+        print('Easy')
+    elif level=='M' or level=='m':
+        print('Medium') 
+    elif level=='H' or level=='h':
+        print('Hard')
+    else:
+        print('Invalid input. Please select E, M, or H.')
