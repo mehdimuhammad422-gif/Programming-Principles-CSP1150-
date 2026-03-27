c2a3cee46c84de0852860fda386ea2d1910c8a16
mehdimuhammad422-gif <mehdimuhammad422@gmail.com>
2026-03-27 17:00:52 +0500

    creating varaibales and setting them as per difficulty level

--- main.py
+++ main.py
@@ -5,13 +5,26 @@ rollNo=input()
 print(f'Welcome to {s_name} ({rollNo}) Number List Test')
 print('Select the difficulty level [E]asy, [M]edium, [H]ard. ')
 level=''
+qNum = 0
+numCount = 0    
+minNum = 0
+maxNum = 0
 while level not in ['E', 'e', 'M', 'm', 'H', 'h']:
     level=input( )
     if level=='E' or level=='e':
-        print('Easy')
+        qNum = 4
+        numCount = 3
+        minNum = 5
+        maxNum = 10
     elif level=='M' or level=='m':
-        print('Medium') 
+        qNum = 5
+        numCount = 4
+        minNum = 5
+        maxNum = 15
     elif level=='H' or level=='h':
-        print('Hard')
+        qNum = 6
+        numCount = 5
+        minNum = 10
+        maxNum = 20
     else:
         print('Invalid input. Please select E, M, or H.')
